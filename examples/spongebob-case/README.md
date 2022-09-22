49cc99385b6937faa94d79457f9a4fa86bf24bde
Daniel M <danielm-github@dnml.de>
2022-09-22 17:16:34 +0200

    Add example project

---
+++ examples/spongebob-case/README.md
@@ -0,0 +1,4 @@
+# Spongebob-Case Hotkey
+> An example project for the `windows-hotkeys` crate
+
+This program adds a hotkey which replaces text in the clipboard with the sPoNgEbOb cAsE RePrEsEnTaTiOn
---
+++ examples/spongebob-case/src/main.rs
@@ -0,0 +1,44 @@
+use clipboard_win::{get_clipboard_string, set_clipboard_string};
+use windows_hotkeys::{
+    keys::{ModKey, VKey},
+    HotkeyManager,
+};
+
+fn main() {
+    // Prepare hotkey CTRL + ALT + K
+    let mod_keys = [ModKey::Ctrl, ModKey::Alt];
+    let main_key = VKey::K;
+
+    // Create the manager
+    let mut hkm = HotkeyManager::new();
+
+    hkm.register(main_key, &mod_keys, || {
+        // Get the current clipboard text
+        let orig = match get_clipboard_string() {
+            Ok(it) => it,
+            Err(_) => {
+                eprintln!("Failed to get clipboard contents");
+                return;
+            }
+        };
+
+        let mut sponge = String::new();
+
+        // Rebuild the clipboard text, but use alternating upper and lower case letters
+        orig.char_indices().for_each(|(i, c)| {
+            if i % 2 == 0 {
+                sponge.push(c.to_ascii_lowercase());
+            } else {
+                sponge.push(c.to_ascii_uppercase());
+            }
+        });
+
+        // Set the clipboard text to the spongebob case string
+        if let Err(e) = set_clipboard_string(&sponge) {
+            eprintln!("Failed to set clipboard contents '{e}'");
+        }
+    })
+    .unwrap();
+
+    hkm.event_loop();
+}
